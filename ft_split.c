4e4ce17dac7416072a09babec265a5087322caa5
Yasmin Bianca Oliveira Neri <yaolivei@car3s4.42barcelona.com>
2023-10-07 19:05:51 +0200

    porfim

--- ft_split.c
+++ ft_split.c
@@ -6,33 +6,89 @@
 /*   By: yaolivei <yaolivei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 02:37:05 by yaolivei          #+#    #+#             */
-/*   Updated: 2023/10/06 23:20:14 by yaolivei         ###   ########.fr       */
+/*   Updated: 2023/10/07 18:57:10 by yaolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-// contar las palabras
-// contar las letras de cada palabra
-// asignar memoria para cada palabra
-// asignar memoria para cada letra
-// copiar cada letra en cada palabra
-// copiar cada palabra en el array
-// devolver el array
-static size_t	ft_countwords(char const *s, char c)
+// "hola que       tal" -> 3
+// " hola que tal" -> 3
+// "hola que tal " -> 3
+// "        "   ' ' -> 0
+// "" ' ' -> 0
+static size_t	ft_count_words(char const *s, char c)
 {
 	int	i;
 	int	count;
 
 	i = 0;
 	count = 0;
-	if (s[i] == '\0')
-		return (0);
-	while (s[i])
+	while (s[i] != '\0')
 	{
-		if ((s[i] != c && s[i + 1] == c) || (s[i] != c && s[i + 1] == '\0'))
+		while (s[i] == c && s[i] != 0)
+			i++;
+		if (s[i] != c && s[i] != '\0')
 			count++;
-		i++;
+		while (s[i] != c && s[i] != 0)
+			i++;
 	}
 	return (count);
 }
-char	**ft_split(char const *s, char c);
+
+static char	**free_matrix(char **matrix, int num_col)
+{
+	int	i;
+
+	i = 0;
+	while (i < num_col)
+	{
+		free(matrix[i]);
+		i++;
+	}
+	free(matrix);
+	return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**matrix;
+	size_t	i;
+	size_t	j;
+	size_t	start;
+
+	i = 0;
+	j = 0;
+	start = 0;
+	matrix = malloc(sizeof(char *) * (ft_count_words(s, c) + 1));
+	if (!matrix)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (i > 0 && s[i] != c && s[i - 1] == c)
+			start = i;
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+		{
+			matrix[j++] = ft_substr(s, start, (i - start +1));
+			if (matrix[j - 1] == NULL)
+				return (free_matrix(matrix, j - 1));
+		}
+		i++;
+	}
+	matrix[j] = NULL;
+	return (matrix);
+}
+/*int main()
+{
+	char **splitted;
+	int i;
+
+	i = 0;
+	splitted = ft_split("hola que tal como estas tu", ' ');
+	while(splitted[i] != NULL)
+	{
+		printf("%s\n", splitted[i]);
+		i++;
+	}
+	return(1);
+
+}*/
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: yaolivei <yaolivei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 00:27:06 by yaolivei          #+#    #+#             */
-/*   Updated: 2023/10/06 02:13:10 by yaolivei         ###   ########.fr       */
+/*   Updated: 2023/10/07 17:55:43 by yaolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	s_len = ft_strlen(s);
 	if (start > s_len)
 		return ((char *)ft_calloc(1, sizeof(char)));
-	if (s_len < len)
-		len = s_len - start + 1;
+	if (s_len - start < len)
+		len = s_len - start;
 	ptr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!ptr)
 		return (NULL);
