5c81f55ae22b6e33616d64aea1e77971e675e622
Yasmin Neri <139879723+yasoli06@users.noreply.github.com>
2023-10-11 15:12:01 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -19,8 +19,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*a;
 	unsigned char	*b;
 
-	a = s1;
-	b = s2;
+	a = (unsigned char)s1;
+	b = (unsigned char)s2;
 	i = 0;
 	while (i < n && a[i] != '\0' && b[i] != '\0')
 	{
