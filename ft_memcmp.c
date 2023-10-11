0e964b5f59d4036bdbd3c41b9b85c156e2ed3fb9
Yasmin Neri <139879723+yasoli06@users.noreply.github.com>
2023-10-11 15:10:49 +0200

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -19,8 +19,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*b;
 
 	i = 0;
-	a = s1;
-	b = s2;
+	a = (unsigned char)s1;
+	b = (unsigned char)s2;
 	while (i < n)
 	{
 		if (a[i] != b[i])
